3010c11ae7fbe2ce7e3054b6d859fc86f21d3c85
Frankie Yan <frankieyan@gmail.com>
2019-11-07 18:27:16 +0800

    Queue requests when offline

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const pendingRequests = [];
+
 document.querySelector("#form").addEventListener("submit", event => {
   const values = Array.from(event.target.elements)
     .filter(element => element.id)
@@ -17,7 +19,15 @@ document.querySelector("#form").addEventListener("submit", event => {
 });
 
 function sendRequestThroughWire({ values }) {
+  const isOnline = navigator.onLine;
+
   return new Promise((resolve, reject) => {
+    if (!isOnline) {
+      window.alert(`You're currently offline! When you regain network connectivity your request will be sent. Putting the following request in the queue now: \n${values}`);
+      pendingRequests.push(() => resolve({ queued: true, values }));
+      return;
+    }
+
     resolve();
   });
 }
